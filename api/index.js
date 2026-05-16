cb96c3ec31ff5195dd2cdb2453ddced3715e6084
Mayibongwe Ngwenya <mayibongwe.ngwenya@entelect.co.za>
2026-05-16 20:42:01 +0200

    remove lambdafy

--- api/index.js
+++ api/index.js
@@ -1,16 +1,8 @@
-import awsLambdaFastify from '@fastify/aws-lambda';
 import { buildApp } from '../dist/app.js';
 
-let proxy;
-
 export default async function handler(req, res) {
-  if (!proxy) {
-    const app = await buildApp();
+  const app = await buildApp();
+  await app.ready();
 
-    proxy = awsLambdaFastify(app);
-
-    await app.ready();
-  }
-
-  return proxy(req, res);
+  app.server.emit('request', req, res);
 }
