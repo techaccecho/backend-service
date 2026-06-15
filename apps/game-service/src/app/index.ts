bcdd43591b963a507938656d136ba044dbacd03b
Mayibongwe Ngwenya <mayibongwe.ngwenya@entelect.co.za>
2026-06-15 23:14:21 +0200

    add dependency

--- apps/game-service/src/app/index.ts
+++ apps/game-service/src/app/index.ts
@@ -1,5 +1,4 @@
 import { bootstrap } from '@lib/starter';
-import { routesPlugin } from '../plugins/index.js';
 
 export async function buildApp() {
   const app = await bootstrap({
@@ -10,8 +9,6 @@ export async function buildApp() {
       version: '1.0.0'
     },
   });
-
-  await app.register(routesPlugin);
 
   return app;
 }
