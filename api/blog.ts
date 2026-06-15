7ad9fdcf13c3a8e5777b7c4752752364bd6946f6
Mayibongwe Ngwenya <mayibongwe.ngwenya@entelect.co.za>
2026-06-15 23:52:49 +0200

    up

--- api/blog.ts
+++
@@ -1,8 +0,0 @@
-import { serverless } from '@lib/starter';
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { buildApp } from '../apps/blog-service/src/app/index.js';
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const app = await buildApp();
-  await serverless(app, req, res);
-}
