476a0b5ffa87e76eeac7c22ff4ad6bff5c6452de
Mayibongwe Ngwenya <mayibongwe.ngwenya@entelect.co.za>
2026-06-15 20:23:18 +0200

    fix: app

---
+++ apps/blog-service/api/index.js
@@ -0,0 +1,7 @@
+import { serverless } from '@lib/starter';
+import { buildApp } from '../dist/app/index.js';
+
+export default async function handler(req, res) {
+  const app = await buildApp();
+  await serverless(app, req, res);
+}
