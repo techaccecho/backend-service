9e7b80e00f3ac935f503688dc44f6b2ad34149b0
Mayibongwe Ngwenya <mayibongwe.ngwenya@entelect.co.za>
2026-06-15 23:09:24 +0200

    fix: update auth

--- apps/auth-service/src/app/index.ts
+++ apps/auth-service/src/app/index.ts
@@ -1,17 +1,14 @@
 import { bootstrap } from '@lib/starter';
-import { routesPlugin } from '../plugins/index.js';
 
 export async function buildApp() {
   const app = await bootstrap({
-    routePrefix: '/blog-api',
+    routePrefix: '/auth-api',
     docs: {
-      title: 'Blog Service',
-      description: 'Documentation of the Blog Service',
+      title: 'Auth Service',
+      description: 'Documentation of the Auth Service',
       version: '1.0.0'
     },
   });
-
-  await app.register(routesPlugin);
 
   return app;
 }
