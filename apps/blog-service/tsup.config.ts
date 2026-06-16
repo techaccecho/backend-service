903a188baadb434ff6686dfbb19da8e12c65b3b7
Mayibongwe Ngwenya <mayibongwe.ngwenya@entelect.co.za>
2026-06-16 18:30:42 +0200

    fix: build

--- apps/blog-service/tsup.config.ts
+++ apps/blog-service/tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/api/index.ts'],
+  outDir: 'api',
   format: ['esm'],
   dts: false,
   tsconfig: 'tsconfig.json',
