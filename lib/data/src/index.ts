3945641cb347695dbb22ce4bc65844ebb6a787cc
Mayibongwe Ngwenya <mayibongwe.ngwenya@entelect.co.za>
2026-07-04 15:53:35 +0200

    chore: add vscode scripts and streamline data integration

--- lib/data/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from '../convex/_generated/api.js';
-export type * from '../convex/_generated/dataModel.js';
-export * from '../convex/_generated/server.js';
-export * from '../convex/schema.js';
